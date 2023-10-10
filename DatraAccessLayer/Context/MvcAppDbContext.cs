e6427e45eb02af5f22955bd50b2fa460ae7f57ac
DELL@DESKTOP-MDCU7M3 <abdomohkhd108@gmail.com>
2023-10-10 13:45:33 +0300

    first commit

---
+++ DatraAccessLayer/Context/MvcAppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DatraAccessLayer.Context
+{
+    public class MvcAppDbContext : DbContext
+    {
+        public MvcAppDbContext(DbContextOptions<MvcAppDbContext> options) :base(options)
+        { 
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var constring = configuration.GetConnectionString("MyConnection");
+            optionsBuilder.UseSqlServer(constring);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# mvcagain
